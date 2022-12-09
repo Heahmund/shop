1ec9da619f1e12cb9e98ecaeccdffc7e8e48eeef
Heahmund <tima260399@mail.ru>
2022-12-09 14:54:21 +0200

    Translation_and_test. Issue #8355

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    validates :title, :description, :image_url, presence: { message: 'Должен быть заполнен' } 
+    validates :title, uniqueness: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01, message: 'Значение некорректно или пусто' } 
+    validates :image_url, allow_blank: true, format: {
+        with: %r{\.(gif|jpg|png)\z}i,
+        message: 'долен быть введен URL для GIF, JPG или PNG изображения.'
+        }
 end
